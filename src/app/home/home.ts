7298ed4148d7b4ad64399bd64355789d17bec2ae
Boris <boris07georgiev@gmail.com>
2025-10-07 14:32:48 +0300

    added the images for the demo version of home page

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-
+  images = [
+    'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/5.jpg',
+    'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/9.jpg',
+    'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/4.jpg',
+    'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/7.jpg',
+    'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/1.jpg',
+    'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/2.jpg'
+  ];
 }
