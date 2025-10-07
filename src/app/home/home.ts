6a7c5449ec9ca82075f03c3d082ffa9cc9f5ea38
Boris047 <boris07georgiev@gmail.com>
2025-10-07 22:44:49 +0300

    created the carousel

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,8 +1,11 @@
 import {Component} from '@angular/core';
+import {Carousel, CarouselModule, CarouselResponsiveOptions} from 'primeng/carousel';
 
 @Component({
   selector: 'app-home',
   imports: [
+    Carousel,
+    CarouselModule
   ],
   templateUrl: './home.html',
   styleUrl: './home.css'
@@ -16,4 +19,5 @@ export class Home {
     'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/1.jpg',
     'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/2.jpg'
   ];
+  responsiveOptions: CarouselResponsiveOptions[] | undefined;
 }
