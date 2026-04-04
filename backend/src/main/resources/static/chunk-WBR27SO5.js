60231652b88ef237bf94b661b159d9231935b387
Boris <boris07georgiev@gmail.com>
2026-04-04 20:32:02 +0300

    feat: add CPU component with service, model, and product list integration generics concept created, but not tested. todo -> CPU socket handling

---
+++ backend/src/main/resources/static/chunk-WBR27SO5.js
@@ -0,0 +1,1 @@
+import{Qb as c,Ya as o,tb as s,ub as n}from"./chunk-6BGGYRIR.js";var r=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=o({type:e,selectors:[["app-products"]],decls:2,vars:0,template:function(t,a){t&1&&(s(0,"p"),c(1,"products works!"),n())},encapsulation:2})};export{r as Products};
